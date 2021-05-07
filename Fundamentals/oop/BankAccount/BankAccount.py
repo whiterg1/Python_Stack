a49d020e2e5d188dad1958797a6b1f28a2de0028
Roy White <roy.g.white8@icloud.com>
2021-05-06 23:19:19 -0500

    initial_commit

--- Fundamentals/oop/BankAccount/BankAccount.py
+++ Fundamentals/oop/BankAccount/BankAccount.py
@@ -1,10 +1,9 @@
 class BankAccount:
     bank_name = "The National Bank of Roy"
     all_accounts = []
-    def __init__(self,name,int_rate,balance):
+    def __init__(self,int_rate,balance):
         self.int_rate = int_rate
         self.balance = balance
-        self.name = name
         BankAccount.all_accounts.append(self)
     
     def deposit(self, amount):
@@ -16,7 +15,7 @@ class BankAccount:
         return self
     
     def display_account_info(self):
-        print(f'Account: {self.name} Has a balance of: ${self.balance}')
+        print(f' This account has a balance of: ${self.balance}')
         return self
     
     def yield_interest(self):
@@ -26,10 +25,24 @@ class BankAccount:
         else:
             print("There are insufficient funds in the account.")
 
-bubbas_bbq = BankAccount("Bubba's BBQ",0.03, 0)
-ginos_barbershop = BankAccount("Gino's Barbershop", 0.06, 0)
+bubbas_bbq = BankAccount(0.03, 0)
+ginos_barbershop = BankAccount(0.06, 0)
 
 bubbas_bbq.deposit(30).deposit(50).deposit(10).withdraw(20).yield_interest().display_account_info()
 ginos_barbershop.deposit(200).deposit(50).withdraw(100).withdraw(10).withdraw(10).withdraw(20).yield_interest().display_account_info()
 
+class User:
+    def __init__(self, name, email_address):
+        self.name = name
+        self.email = email_address
+        self.account = BankAccount(0.01, 0)
+    def make_deposit(self, amount):
+        self.account.deposit(amount)
+    def make_withdrawal(self, amount):
+        self.account.withdraw(amount)
 
+john = User("John Wick", "Babayaga1@gmail.com")
+beatrix = User("Beatrix Kiddo", "The_bride@gmail.com")
+thomas = User("Thomas Anderson", "The_one@gmail.com")
+
+john.account.deposit(15).display_account_info()
