4e58e0b716bee2ad8640475e147f4343154fd387
Roy White <roy.g.white8@icloud.com>
2021-05-07 20:26:08 -0500

    initial_commit

--- Fundamentals/oop/BankAccount/BankAccount.py
+++ Fundamentals/oop/BankAccount/BankAccount.py
@@ -30,19 +30,3 @@ ginos_barbershop = BankAccount(0.06, 0)
 
 bubbas_bbq.deposit(30).deposit(50).deposit(10).withdraw(20).yield_interest().display_account_info()
 ginos_barbershop.deposit(200).deposit(50).withdraw(100).withdraw(10).withdraw(10).withdraw(20).yield_interest().display_account_info()
-
-class User:
-    def __init__(self, name, email_address):
-        self.name = name
-        self.email = email_address
-        self.account = BankAccount(0.01, 0)
-    def make_deposit(self, amount):
-        self.account.deposit(amount)
-    def make_withdrawal(self, amount):
-        self.account.withdraw(amount)
-
-john = User("John Wick", "Babayaga1@gmail.com")
-beatrix = User("Beatrix Kiddo", "The_bride@gmail.com")
-thomas = User("Thomas Anderson", "The_one@gmail.com")
-
-john.account.deposit(15).display_account_info()
