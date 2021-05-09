53b9c3c652dd0eef07366e48ad2f9ae83faa0a93
Roy White <roy.g.white8@icloud.com>
2021-05-09 11:14:48 -0500

    initial_commit

--- Flask/Fundamentals/hello_flask/hello.py
+++ Flask/Fundamentals/hello_flask/hello.py
@@ -1,2 +1,20 @@
-<built-in function dir>
-<built-in function dir>
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello World!"
+
+@app.route("/success")
+def success():
+    return "success"
+
+@app.route("/hello/<name>")
+def hello(name):
+    return "Hello, " + name
+
+
+if __name__ == "__main__":
+    app.run()
+
---
+++ Flask/Fundamentals/understanding_routing/understanding_routing.py
@@ -0,0 +1,28 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello World!"
+
+@app.route("/dojo")
+def dojo():
+    return "Dojo!"
+
+@app.route("/say/<name>")
+def say(name):
+    print(name)
+    return f"Hi, {name}!"
+
+@app.route("/repeat/<int:number>/<message>")
+def repeat(number, message):
+    return f"{message * number}"
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
+
+
+
+
