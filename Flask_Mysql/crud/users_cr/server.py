6d4327c3a5bc5d6b7ef52551f199c429da98f7cf
Roy White <roy.g.white8@icloud.com>
2021-05-14 21:35:32 -0500

    initial_commit

--- Flask_Mysql/crud/users_cr/server.py
+++ Flask_Mysql/crud/users_cr/server.py
@@ -1,11 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from mysqlconnection import connectToMySQL
 app=Flask(__name__)
 
-@app.route("/")
-def index()
+@app.route("/users")
+def index():
+    mysql = connectToMySQL("users_schema")
+    query = "SELECT * FROM users;"
+    users = mysql.query_db(query)
+    return render_template("Read(all).html", all_users = users)
 
 
+@app.route("/users/new")
+def new_user_form():
+    return render_template("Create.html")
 
-if __name__=="__main__":
+@app.route("/users/create", methods = ["POST"])
+def new_user():
+    mysql = connectToMySQL("users_schema")
+    query = "INSERT INTO users(first_name, last_name, email, created_at, updated_at) \
+    VALUES(%(first_name)s,%(last_name)s,%(email)s,NOW(), NOW());"
+    data = {
+        "first_name": request.form['first_name'],
+        "last_name": request.form['last_name'],
+        "email": request.form['email']
+    }
+    user_id = mysql.query_db(query, data)
+    
+    return redirect("/users")
+
+if __name__ == "__main__":
     app.run(debug=True)
