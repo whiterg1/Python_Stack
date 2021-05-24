ff1a44794fe0303ae6cea6d9f26f90dd9fd24983
Roy White <roy.g.white8@icloud.com>
2021-05-24 13:32:10 -0500

    initial_commit

--- Flask/Fundamentals/checkerboard/server.py
+++ Flask/Fundamentals/checkerboard/server.py
@@ -5,16 +5,13 @@ app = Flask(__name__)
 def first_checkerboard():
     return render_template("index.html")
 
-@app.route("/<x>")
+@app.route("/<int:x>")
 def second_checkerboard(x):
-    x = int(x)
-    return render_template("index.html", x = x)
+    return render_template("index2.html", x = x)
 
-@app.route("/<x>/<y>")
+@app.route("/<int:x>/<int:y>")
 def third_checkboard(x,y):
-    x = int(x)
-    y = int(y)
-    return render_template("index.html", x = x, y = y)
+    return render_template("index3.html", x = x, y = y)
 
 if __name__=="__main__":
     app.run(debug = True)
